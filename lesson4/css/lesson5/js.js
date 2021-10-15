1b39196fd324f889b68d6a4d36be1e10eb1777c8
MosesEtte0721 <ett20002@byui.edu>
2021-10-15 01:17:25 +0100

    Initial Commit

---
+++ lesson4/css/lesson5/js.js
@@ -0,0 +1,16 @@
+const input;
+const button;
+const list;
+input = document.querySelector("section");
+list = document.createElement("input");
+
+const ul;
+const input;
+const button;
+function button(){
+  document.addEventListener("onclick")
+  input = ""
+  const li;
+  const span;
+  const buton;
+}

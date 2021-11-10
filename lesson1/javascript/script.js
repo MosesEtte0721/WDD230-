90a92174db47e55e0413d5006ace71a2559c94f6
MosesEtte0721 <ett20002@byui.edu>
2021-11-10 10:35:45 +0100

    Initial Commit

---
+++ lesson1/javascript/script.js
@@ -0,0 +1,16 @@
+// let time = new Date();
+// let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+// let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  
+
+// let date = time.getDay();
+// let day = days[time.getDay()];
+// let numero = time.getMonth();
+// let month = months[time.getMonth()];
+// let years = time.getFullYear();
+// let all = `Last updated: ${date} / ${month} / ${years}`;
+ 
+// document.getElementById("time").innerHTML = all
+
+var latestDate = new Date(document.lastModified);
+document.getElementById("time").innerHTML = latestDate;

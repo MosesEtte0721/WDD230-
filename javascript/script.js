6d57317f08c7f3ce8796e0be9edffd34465c7756
MosesEtte0721 <ett20002@byui.edu>
2021-11-15 23:52:41 +0100

    Initial Commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-// var latestDate = new Date(document.lastModified);
-// // document.getElementById("times").innerHTML = latestDate;
-// console.log(latestDate)
-// document.getElementById("timebomb").innerHTML =latestDate;
+var latestDate = new Date(document.lastModified);
+// document.getElementById("times").innerHTML = latestDate;
+console.log(latestDate)
+document.getElementById("timebomb").innerHTML =latestDate;
--- lesson6/js-6/js.js
+++
@@ -1,5 +0,0 @@
-function toggleMenu() {
-   
-    document.getElementById("javascript").classList.toggle("togglemenu");
-    } 
-  

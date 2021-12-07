d0bd160931c64f6b91dbf66dc113991bb011da2d
MosesEtte0721 <ett20002@byui.edu>
2021-12-07 08:28:28 +0100

    Initial Commit

--- lesson8/javascript8/toggle.js
+++ lesson8/javascript8/toggle.js
@@ -40,5 +40,15 @@ function ToggleMenu() {
       
   
     
-  
+      function adjustRating(rating) {
+        document.getElementById("ratingvalue").innerHTML = rating;
+    }
+    
+    function selectResponse() {
+      const s = document.querySelector('#selected')
+      const sel = document.querySelector('#selectbrowser');
+      s.style.display = "block";
+      s.textContent = sel.value;
+      
+    }
   

ed23def7ebc863804b15fa684a5cebe34137c105
MosesEtte0721 <ett20002@byui.edu>
2021-12-17 15:15:47 +0100

    Initial Commit

--- directory.js
+++ directory.js
@@ -2,24 +2,6 @@ const gridList = document.querySelector('.grid-list-views');
 const gridView = document.querySelector('.grid-view');
 const listView = document.querySelector('.list-view');
 
-// const loadJson = loadJson("directory.json");
-
-
-
-// fetch(loadJson('directory.json')).then( res => {
-//     return res.json()
-// }).then(ask => {
-//     console.log(ask);
-// });
-
-// function listView() {
-
-//     // fet
-// }
-
-// let get = $.getJSON("directory.json") ;
-// const js = fetch("directory.json");
-// console.log(js);
 
 fetch("directory.json").then(res=> {
     return res.json();
@@ -31,15 +13,20 @@ fetch("directory.json").then(res=> {
     let gridList = document.querySelector('.grid-list-views');
     let section = document.createElement('section');
     let name = document.createElement('p');
-    let address =document.createElement('p');
+    let address = document.createElement('p');
     let desc = document.createElement('p');
     let email = document.createElement('p');
+    let image = document.createElement('img');
 
     name.textContent = `Name: ${list.name}`;
     address.textContent = `Address: ${list.address}`;
     desc.textContent = `Description: ${list.description}`;
     email.textContent = `Email: ${list.email}`;
 
+    image.setAttribute('src', `${list.image}`);
+    image.setAttribute('alt',`${list.name}`);
+
+    section.appendChild(image);
     section.appendChild(name);
     section.appendChild(address);
     section.appendChild(desc);

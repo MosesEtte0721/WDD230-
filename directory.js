b7baac8e456e120dfdc788522708accaa9872594
MosesEtte0721 <ett20002@byui.edu>
2021-12-17 09:47:16 +0100

    Initial Commit

--- directory.js
+++ directory.js
@@ -26,7 +26,23 @@ fetch("directory.json").then(res=> {
 }).then(ask=> {
     console.log(ask);
    ask.forEach(element => {
-    let div = document.createElement('p');
+    let gridList = document.querySelector('.grid-list-views');
+    let section = document.createElement('section');
+    let name = document.createElement('p');
+    let address =document.createElement('p');
+    let desc = document.createElement('p');
+    let email = document.createElement('p');
+    name.textContent = `Name: ${element.name}`;
+    address.textContent = `Address: ${element.addres}`;
+    desc.textContent = `Description: ${element.desc}`;
+    email.textContent = `Email: ${element.email}`;
+
+    section.appendChild(name);
+    section.appendChild(address);
+    section.appendChild(desc);
+    section.appendChild(email);
+    gridList.appendChild(section);
+
     listView.textContent = element.com1.Address;
     console.log(listView.textContent = element)
     listView.appendChild(div);
